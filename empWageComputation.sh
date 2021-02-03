5a85339b1ae29d011cc359966f25480b5d515017
omkarmali4437 <omkarmalli688@gmail.com>
2021-02-03 17:00:00 +0530

    Added logic for calculating daily wage of a employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,18 @@
 
 echo "Welcome to Employee Wage computation Problem"
 
+Wageperhour=20;
+
 randomcheck=$(( RANDOM%2 ))
 if [[ $randomcheck -eq 1 ]]
 then
 	echo "Employee is Present"
+	employeehrs=8;
+	Dailywage=$(($Wageperhour*$employeehrs))
+	echo $Dailywage
 else
 	echo "Employee is Absent"
+	employeehrs=0;
+	Dailywage=$(($Wageperhour*$employeehrs))
+	echo $Dailywage
 fi
